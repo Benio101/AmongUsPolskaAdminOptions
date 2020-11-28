16f126f545da0ab94341d11ede9ee2cd077be9bb
Paweł Benetkiewicz <git@benio.me>
2020-11-28 17:18:12 +0100

    Update AmongUsPolskaAdminOptions.plugin.js

--- AmongUsPolskaAdminOptions.plugin.js
+++ AmongUsPolskaAdminOptions.plugin.js
@@ -140,6 +140,7 @@ module.exports = (() =>
 		nomedia: `<svg height='24px' width='24px' fill="#FFFF40" data-name="Laag 1" viewBox="0 0 100 100" x="0px" y="0px"><polygon points="75.91 78.29 74.81 77.28 73.73 76.29 26.51 76.29 25.43 77.28 24.33 78.29 24.2 78.41 24.2 79.61 77.35 79.61 75.91 78.29"></polygon><polygon points="79.01 28.17 78.01 29.09 78.01 70.17 79.01 71.08 80.01 72 81.34 73.22 81.34 26.04 80.01 27.26 79.01 28.17"></polygon><polygon points="77.48 19.52 78.57 18.52 21.67 18.52 22.76 19.52 23.86 20.52 76.38 20.52 77.48 19.52"></polygon><polygon points="22.25 70.14 22.25 29.11 21.25 28.19 20.25 27.27 20.25 71.98 21.25 71.06 22.25 70.14"></polygon><path d="M65.82,27a4.87,4.87,0,0,1,2.66.8l3.91-3.59H27.85l16.41,15L47,33.8l5.52,8.57,9-8.28A4.83,4.83,0,0,1,65.82,27Z"></path><polygon points="39.88 47.78 40.76 46.07 25.28 31.88 25.28 57.72 28.05 57.72 36.93 43.28 39.88 47.78"></polygon><polygon points="74.36 57.72 74.36 32.44 63.38 42.5 72.93 57.72 74.36 57.72"></polygon><polygon points="88.85 10.36 79.95 18.52 78.86 19.52 77.77 20.52 75.98 22.16 75.81 22.31 75.64 22.47 74.11 23.87 73.95 24.02 73.78 24.18 69.21 28.37 62.12 34.87 53.07 43.16 51.01 45.05 50.12 45.86 49.23 45.05 43.82 40.09 26.46 24.18 26.29 24.02 26.13 23.87 24.6 22.47 24.43 22.31 24.26 22.16 22.47 20.52 21.38 19.52 20.29 18.52 11.39 10.36 7.99 7.24 4.24 11.33 20.25 26.01 21.25 26.93 22.25 27.84 23.89 29.35 24.04 29.48 24.2 29.63 24.97 30.34 25.13 30.48 25.28 30.62 41.19 45.21 45.12 48.81 46.02 49.62 37.18 57.72 37.02 57.87 36.85 58.03 34.56 60.12 24.2 69.62 24.04 69.77 23.89 69.91 22.25 71.41 21.25 72.33 20.25 73.24 8.45 84.06 11.39 87.26 12.2 88.15 22.96 78.29 23.89 77.43 24.04 77.3 24.06 77.28 24.2 77.15 25.14 76.29 42.86 60.04 45.06 58.03 45.23 57.87 45.4 57.72 50.12 53.39 54.84 57.72 55.01 57.87 55.18 58.03 57.23 59.9 75.1 76.29 76.18 77.28 77.28 78.29 78.72 79.61 78.9 79.77 79.06 79.92 88.04 88.15 88.85 87.26 91.79 84.06 81.65 74.76 81.5 74.63 81.34 74.48 80.01 73.26 79.01 72.34 78.01 71.43 65.35 59.83 63.39 58.03 63.22 57.87 63.06 57.72 54.22 49.62 55.12 48.81 62.88 41.7 74.36 31.17 74.51 31.03 74.67 30.89 78.01 27.82 79.01 26.91 80.01 25.99 81.34 24.77 81.5 24.62 81.65 24.49 96 11.33 92.25 7.24 88.85 10.36"></polygon></svg>`,
 		checkbox_cross: `<svg height='24px' width='24px' fill="#FFFF40" x="0px" y="0px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;"><path d="M80.8,5H19.3C11.4,5,5,11.4,5,19.3v61.5C5,88.6,11.4,95,19.3,95h61.5C88.6,95,95,88.6,95,80.8V19.3C95,11.4,88.6,5,80.8,5z M85,80.8c0,2.3-1.9,4.3-4.3,4.3H19.3c-2.3,0-4.3-1.9-4.3-4.3V19.3c0-2.3,1.9-4.3,4.3-4.3h61.5c2.3,0,4.3,1.9,4.3,4.3V80.8z M69.9,61.6c2,1.9,2.2,5,0.3,7.1c-1,1.1-2.3,1.6-3.7,1.6c-1.2,0-2.4-0.4-3.4-1.3L50.3,57.1L38.4,69.9c-1,1.1-2.3,1.6-3.7,1.6 c-1.2,0-2.4-0.4-3.4-1.3c-2-1.9-2.2-5-0.3-7.1l11.9-12.9L30.1,38.4c-2-1.9-2.2-5-0.3-7.1c1.9-2,5-2.2,7.1-0.3l12.9,11.9l11.9-12.9 c1.9-2,5-2.2,7.1-0.3c2,1.9,2.2,5,0.3,7.1L57.1,49.7L69.9,61.6z"></path></svg>`,
 		banhammer: `<svg height='24px' width='24px' fill="#FF4040" x="0px" y="0px" viewBox="0 0 80 80" enable-background="new 0 0 80 80"><path d="M28.832,70.75l-0.542-2C27.693,66.542,25.604,65,23.208,65H8.792c-2.395,0-4.485,1.542-5.082,3.75l-0.542,2 c-0.394,1.454-0.087,2.983,0.84,4.195C4.992,76.232,6.578,77,8.25,77h15.5c1.672,0,3.258-0.768,4.242-2.055 C28.919,73.733,29.226,72.204,28.832,70.75z"></path><path d="M9,17c-1.104,0-2-0.896-2-2v-4c0-1.104,0.896-2,2-2s2,0.896,2,2v4C11,16.104,10.104,17,9,17z"></path><circle cx="9" cy="5" r="2"></circle><path d="M23,11c-1.104,0-2-0.896-2-2V5c0-1.104,0.896-2,2-2s2,0.896,2,2v4C25,10.104,24.104,11,23,11z"></path><circle cx="23" cy="15" r="2"></circle><path d="M72,30c-2.045,0-3.802,1.237-4.576,3H27v-8c0-3.309-2.691-6-6-6H11c-3.309,0-6,2.691-6,6v30c0,3.309,2.691,6,6,6h10 c3.309,0,6-2.691,6-6v-8h40.424c0.774,1.763,2.531,3,4.576,3c2.757,0,5-2.243,5-5V35C77,32.243,74.757,30,72,30z M21,55H11 c-1.104,0-2-0.896-2-2s0.896-2,2-2h10c1.104,0,2,0.896,2,2S22.104,55,21,55z M21,29H11c-1.104,0-2-0.896-2-2s0.896-2,2-2h10 c1.104,0,2,0.896,2,2S22.104,29,21,29z M49.789,39.895l-1,2C48.438,42.596,47.732,43,46.999,43c-0.301,0-0.606-0.067-0.893-0.211 c-0.988-0.494-1.388-1.695-0.895-2.684l1-2c0.495-0.988,1.697-1.388,2.684-0.895C49.882,37.705,50.283,38.906,49.789,39.895z M56.789,39.895l-1,2C55.438,42.596,54.732,43,53.999,43c-0.301,0-0.606-0.067-0.893-0.211c-0.988-0.494-1.388-1.695-0.895-2.684 l1-2c0.495-0.988,1.696-1.388,2.684-0.895C56.882,37.705,57.283,38.906,56.789,39.895z M63.789,39.895l-1,2 C62.438,42.596,61.732,43,60.999,43c-0.301,0-0.606-0.067-0.893-0.211c-0.988-0.494-1.388-1.695-0.895-2.684l1-2 c0.495-0.988,1.697-1.388,2.684-0.895C63.882,37.705,64.283,38.906,63.789,39.895z"></path></svg>`,
+		link: `<svg height='24px' width='24px'  fill="#FFFF40" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><path d="M57.041,68.077L43.288,81.831c-6.919,6.919-18.157,6.951-25.117,0.097l-0.001,0.001c-0.016-0.016-0.032-0.033-0.048-0.05  c-0.016-0.016-0.033-0.032-0.05-0.048c0,0,0,0,0,0l0.001-0.001c-6.854-6.96-6.823-18.198,0.097-25.117l15.156-15.156  c6.919-6.919,18.157-6.951,25.117-0.097l0.001-0.001c0.016,0.016,0.032,0.033,0.048,0.049c0.016,0.016,0.033,0.032,0.05,0.049  l-0.001,0.001c0.832,0.845,1.563,1.752,2.192,2.707l-4.118,4.118c-1.174,1.174-2.962,1.363-4.338,0.575  c-0.181-0.228-0.375-0.448-0.584-0.66c-3.21-3.152-8.385-3.136-11.573,0.052L24.963,63.506c-3.19,3.19-3.205,8.368-0.048,11.578  c3.21,3.157,8.389,3.142,11.578-0.048l8.615-8.615C48.885,68.059,53.022,68.61,57.041,68.077z M81.831,18.071l-0.001,0.001  c-6.96-6.854-18.198-6.823-25.117,0.097L42.959,31.923c4.02-0.533,8.157,0.018,11.933,1.655l8.615-8.615  c3.19-3.19,8.368-3.205,11.578-0.048c3.157,3.21,3.142,8.389-0.048,11.578L59.881,51.65c-3.188,3.188-8.363,3.204-11.573,0.052  c-0.208-0.212-0.403-0.432-0.584-0.66c-1.376-0.788-3.164-0.599-4.338,0.575l-4.118,4.118c0.629,0.955,1.36,1.863,2.192,2.707  l-0.001,0.001c0.016,0.016,0.033,0.032,0.05,0.049c0.016,0.016,0.032,0.033,0.048,0.049l0.001-0.001  c6.96,6.854,18.198,6.823,25.117-0.097l15.156-15.156c6.919-6.919,6.951-18.157,0.097-25.117l0.001-0.001l0,0  c-0.016-0.016-0.033-0.032-0.05-0.048C81.863,18.104,81.847,18.088,81.831,18.071z"></path></svg>`,
 		AD: `<svg height='24px' width='24px' fill="#FF4040" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><g><text x="0" y="75" style="font: bold 70px sans-serif;">AD</text></g></svg>`,
 		SWEARING: `<svg height='24px' width='24px' fill="#FFFF40" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100"><g><text x="0" y="80" style="font: bold 90px sans-serif;">%!</text></g></svg>`,
 	}
@@ -151,36 +152,36 @@ module.exports = (() =>
 			name: 'AmongUsPolskaAdminOptions',
 			description: 'Dodatkowe opcje dla Administracji serwera Among Us Polska.',
 			author: 'Benio',
-			version: '0.2.4',
-			github: 'https://github.com/1Lighty',
-			github_raw: 'https://raw.githubusercontent.com/1Lighty/BetterDiscordPlugins/master/Plugins/1XenoLib.plugin.js'
+			version: '0.3',
+			github: 'https://github.com/Benio101',
+			github_raw: 'https://raw.githubusercontent.com/Benio101/AmongUsPolskaAdminOptions/main/AmongUsPolskaAdminOptions.plugin.js'
 		},
 
 		changeLog:
 		{
 			fixed:
 			{
-				'Aktualizacja': 'Od teraz plugin powinien się sam aktualizować',
+				
 			},
 			improved:
 			{
-				'Kropka': 'Dodano brakującą kropkę po powodzie warna "Usunięte ostrzeżenia"',
+				
 			}
 		},
 
 		time:
 		{
-			/* milliseconds */ starting_delay:                   250,
-			/* milliseconds */ tick_interval_min:                500,
-			/* milliseconds */ tick_interval_max:                1e3,
-			/* milliseconds */ sending_message_timeout_min:      2e3,
-			/* milliseconds */ sending_message_timeout_max:      4e3,
-			/* milliseconds */ deleting_message_timeout_min:     1e3,
-			/* milliseconds */ deleting_message_timeout_max:     2e3,
-			/* milliseconds */ sending_command_timeout_min:      2e3,
-			/* milliseconds */ sending_command_timeout_max:      4e3,
-			/* milliseconds */ busy_waiting_for_bot_timeout:    60e3,
-			/* milliseconds */ unbusy_waiting_for_bot_timeout: 600e3,
+			/* milliseconds */ starting_delay:                    250,
+			/* milliseconds */ tick_interval_min:                 500,
+			/* milliseconds */ tick_interval_max:                 1e3,
+			/* milliseconds */ sending_message_timeout_min:       2e3,
+			/* milliseconds */ sending_message_timeout_max:       4e3,
+			/* milliseconds */ deleting_message_timeout_min:      1e3,
+			/* milliseconds */ deleting_message_timeout_max:      2e3,
+			/* milliseconds */ sending_command_timeout_min:       2e3,
+			/* milliseconds */ sending_command_timeout_max:       4e3,
+			/* milliseconds */ waiting_for_dyno_timeout:         60e3,
+			/* milliseconds */ waiting_for_user_warns_timeout: 3600e3,
 		},
 	};
 
@@ -211,21 +212,30 @@ module.exports = (() =>
 	}
 
 	// ------------------------------------------------------------------------------------------------------------
-	// ------------------------------------ Main Loop -------------------------------------------------------------
+	// ------------------------------------ Variables -------------------------------------------------------------
 	// ------------------------------------------------------------------------------------------------------------
+
+	let GBDFDB = null;
+	let user_warnings = [];
+	let dyno_busy = false;
 
 	let main_timer = null;
 	let dyno_timer = null;
-	let dyno_busy = false;
-	let GBDFDB = null;
 
-	let main = function()
+	let main_queue = new queue;
+	let dyno_queue = new queue;
+
+	// ------------------------------------------------------------------------------------------------------------
+	// ------------------------------------ Main Loop -------------------------------------------------------------
+	// ------------------------------------------------------------------------------------------------------------
+
+	let main_loop = function()
 	{
 		let delay = config.time.tick_interval_min + (Math.max(0, config.time.tick_interval_max - config.time.tick_interval_min) * Math.random());
 
-		if (task_queue.length > 0)
+		if (main_queue.length > 0)
 		{
-			let task = task_queue.shift();
+			let task = main_queue.shift();
 			if (task.action == 'send_message')
 			{
 				tasks.send_message(task.channel_id, task.message);
@@ -243,51 +253,45 @@ module.exports = (() =>
 				tasks.execute_command(task.command);
 				delay += config.time.sending_command_timeout_min + (Math.max(0, config.time.sending_command_timeout_max - config.time.sending_command_timeout_min) * Math.random());
 			}
-			else
+		}
+
+		main_timer = setTimeout(main_loop, delay);
+	}
+
+	// ------------------------------------------------------------------------------------------------------------
+	// ------------------------------------ Dyno Loop -------------------------------------------------------------
+	// ------------------------------------------------------------------------------------------------------------
+
+	let dyno_loop = function()
+	{
+		let delay = config.time.tick_interval_min + (Math.max(0, config.time.tick_interval_max - config.time.tick_interval_min) * Math.random());
+
+		if (dyno_queue.length > 0 && !dyno_busy)
+		{
+			let task = dyno_queue.shift();
 			if (task.action == 'execute_dyno_command')
 			{
-				if (dyno_busy)
-				{
-					let dyno_tasks = new queue;
-					let other_tasks = new queue;
-
-					while (task_queue.length > 0)
-					{
-						let queued_task = task_queue.shift();
-						if (queued_task.action == 'execute_dyno_command')
-							dyno_tasks.push(queued_task);
-						else
-							other_tasks.push(queued_task);
-					}
-
-					while (other_tasks.length > 0)
-						task_queue.push(other_tasks.shift());
-					task_queue.push(task);
-					while (dyno_tasks.length > 0)
-						task_queue.push(dyno_tasks.shift());
-				}
-				else
-				{
-					tasks.execute_dyno_command(task.command);
-					delay += config.time.sending_command_timeout_min + (Math.max(0, config.time.sending_command_timeout_max - config.time.sending_command_timeout_min) * Math.random());
-				}
+				tasks.execute_dyno_command(task.command);
+				delay += config.time.sending_command_timeout_min + (Math.max(0, config.time.sending_command_timeout_max - config.time.sending_command_timeout_min) * Math.random());
 			}
 		}
 
-		main_timer = setTimeout(main, delay);
+		dyno_timer = setTimeout(dyno_loop, delay);
 	}
 
 	// ------------------------------------------------------------------------------------------------------------
 	// ------------------------------------ Tasks -----------------------------------------------------------------
 	// ------------------------------------------------------------------------------------------------------------
 
-	let task_queue = new queue;
 	let add_task = function(task)
 	{
 		if (!task || !task.action)
 			return;
-		
-		task_queue.push(task);
+
+		if (task.action == 'execute_dyno_command')
+			dyno_queue.push(task);
+		else
+			main_queue.push(task);
 	}
 
 	let tasks =
@@ -317,7 +321,7 @@ module.exports = (() =>
 			dyno_busy = true;
 			dyno_timer = setTimeout(function(){
 				dyno_busy = false;
-			}, config.time.busy_waiting_for_bot_timeout);
+			}, config.time.waiting_for_dyno_timeout);
 		},
 	}
 
@@ -345,19 +349,18 @@ module.exports = (() =>
 		add_task({action: 'execute_dyno_command', command: command});
 	}
 
-	let user_warnings = [];
 	let warn = function(user_id, reason)
 	{
 		execute_dyno_command(`!warn ${user_id} ${reason}`);
 		execute_dyno_command(`!warnings ${user_id}`);
 
-		if (user_warnings[user_id])
+		if (user_warnings[user_id] !== undefined)
 			clearTimeout(user_warnings[user_id].timer);
 
 		user_warnings[user_id] = {timer: null};
 		let timer = setTimeout(function(){
 			user_warnings[user_id] = undefined;
-		}, config.time.unbusy_waiting_for_bot_timeout);
+		}, config.time.waiting_for_user_warns_timeout);
 		user_warnings[user_id].timer = timer;
 	}
 
@@ -367,8 +370,8 @@ module.exports = (() =>
 			execute_dyno_command(`!ban ${user_id} ${reason}`);
 		else
 		{
-			perm_mute(user_id);
 			report_to_perm_ban(user_id, reason);
+			perm_mute(user_id);
 		}
 	}
 
@@ -423,27 +426,27 @@ module.exports = (() =>
 			{
 				this.patchedModules = {
 					before: {
-						Message: "default",
+						Message: 'default',
 					},
 				};
 			}
 
 			onStart()
 			{
-				main_timer = setTimeout(main, config.time.starting_delay);
+				main_timer = setTimeout(main_loop, config.time.starting_delay);
+				dyno_timer = setTimeout(dyno_loop, config.time.starting_delay);
 			}
 
 			onStop()
 			{
 				clearTimeout(main_timer);
+				clearTimeout(dyno_timer);
 
 				for (let user_warning of user_warnings)
 					if (user_warning.timer)
 						clearTimeout(user_warning.timer);
 
-				if (dyno_busy)
-					clearTimeout(dyno_timer);
-
+				user_warnings = [];
 				dyno_busy = false;
 			}
 
@@ -616,6 +619,7 @@ module.exports = (() =>
 					&&	(
 								channel.id == channels.chat_ogolny
 							||	channel.id == channels.pomoc_supportu
+							||	channel.id == channels.izolatka_chat
 						)
 				) children.unshift(BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.TooltipContainer,
 				{
@@ -634,6 +638,36 @@ module.exports = (() =>
 						{
 							className: BDFDB.disCN.messagetoolbaricon,
 							iconSVG: images.trash
+						})
+					})
+				}));
+
+				// Link
+				if
+				(		!expanded
+					&&	author.id != BDFDB.UserUtils.me.id
+					&&	(
+								channel.id == channels.chat_ogolny
+							||	channel.id == channels.pomoc_supportu
+							||	channel.id == channels.izolatka_chat
+						)
+				) children.unshift(BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.TooltipContainer,
+				{
+					key: 'mention',
+					text: 'Link',
+					className: 'AmongUsPolskaAdminOptions-button',
+					children: BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.Clickable,
+					{
+						className: BDFDB.disCN.messagetoolbarbutton,
+						onClick: _ =>
+						{
+							delete_message(channel.id, message.id);
+							warn(author.id, `Wysyłanie linków w miejscu do tego nieprzeznaczonym, tj. na kanale <#${channel.id}>.`);
+						},
+						children: BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.SvgIcon,
+						{
+							className: BDFDB.disCN.messagetoolbaricon,
+							iconSVG: images.link
 						})
 					})
 				}));
@@ -825,7 +859,6 @@ module.exports = (() =>
 
 				if (channel_id == channels.komendy && author.bot && author.username == 'Dyno')
 				{
-					clearTimeout(dyno_timer);
 					dyno_busy = false;
 
 					if
@@ -848,19 +881,39 @@ module.exports = (() =>
 					let m = embed.author.name.match(/(\d+) Warnings? for .*? \((\d+)\)/);
 					if (m)
 					{
-						let number_of_warns = m[1];
+						let number_of_warns = 0| m[1];
 						let warned_user_id = m[2];
 
-						if (user_warnings[warned_user_id])
+						if (user_warnings[warned_user_id] !== undefined)
 						{
 							clearTimeout(user_warnings[warned_user_id].timer);
 							user_warnings[warned_user_id] = undefined;
 
-							if (0|number_of_warns < 3)
+							if
+							(		'fields' in embed
+								&&	Array.isArray(embed.fields)
+								&&	embed.fields.length
+								&&	'rawValue' in embed.fields[0]
+								&&	embed.fields[0].rawValue === String(embed.fields[0].rawValue)
+							){
+								let warn_reason = embed.fields[0].split(' - ');
+								if (Array.isArray(warn_reason))
+								{
+									let m = warn_reason[0].match(/(\d+)/);
+									if (m && warn_reason[0].includes('warn') || warn_reason[0].includes('ostrzeż'))
+									{
+										let number_of_prev_warns = 0| m[1];
+										if (number_of_prev_warns > 0 && number_of_prev_warns < 100)
+											number_of_warns += number_of_prev_warns;
+									}
+								}
+							}
+
+							if (number_of_warns < 3)
 								return;
 
 							let reason;
-							if (0|number_of_warns == 3)
+							if (number_of_warns == 3)
 								reason = "Osiągnięto limit ostrzeżeń (3).";
 							else
 								reason = `Przekroczono limit ostrzeżeń (${number_of_warns}/3).`;
